85dfe81d0a5d73b77ff0a23231a5a4af1dbdc666
Marcus Pernow <marcus.pernow@cubist.eu>
2024-12-02 09:12:42 +0100

    simplify day01

--- 2024/src/01/sol.py
+++ 2024/src/01/sol.py
@@ -2,47 +2,20 @@ import pathlib
 
 INPUT_PATH = pathlib.Path(__file__).parent.parent.parent / "input"
 
+
+def get_lists():
+    data = [*map(int, open(INPUT_PATH / "01").read().split())]
+    return sorted(data[0::2]), sorted(data[1::2])
+
+
 def part1():
-    with open(INPUT_PATH / "01") as f:
-        data = f.readlines()
-    data = [line.strip("\n").split(" ") for line in data]
-    list1 = [int(line[0]) for line in data]
-    list2 = [int(line[-1]) for line in data]
+    list1, list2 = get_lists()
+    print(sum(map(lambda a, b: abs(a - b), list1, list2)))
 
-    list1.sort()
-    list2.sort()
-
-    diff = 0
-    for i in range(len(list1)):
-        diff += abs(list1[i] - list2[i])
-    
-    print(diff)
 
 def part2():
-    with open(INPUT_PATH / "01") as f:
-        data = f.readlines()
-    data = [line.strip("\n").split(" ") for line in data]
-    list1 = [int(line[0]) for line in data]
-    list2 = [int(line[-1]) for line in data]
-
-    freq1 = {}
-    for num in list1:
-        if num in freq1.keys():
-            freq1[num] += 1
-        else:
-            freq1[num] = 1
-    freq2 = {}
-    for num in list2:
-        if num in freq2.keys():
-            freq2[num] += 1
-        else:
-            freq2[num] = 1
-
-    score = 0
-    for key, val in freq1.items():
-        score += key*val*freq2.get(key, 0)
-
-    print(score)
+    list1, list2 = get_lists()
+    print(sum(map(lambda a: a * list2.count(a), list1)))
 
 
 if __name__ == "__main__":
